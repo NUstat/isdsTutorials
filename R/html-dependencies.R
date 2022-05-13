9d054401a8e20b769c3dee73e0f6b7150f7d9150
dsass1 <dsass1@comcast.net>
2022-05-13 08:18:31 -0500

    update

--- R/html-dependencies.R
+++ R/html-dependencies.R
@@ -2,7 +2,7 @@
 #' Tutorial HTML dependency
 #'
 #' @details HTML dependency for core tutorial JS and CSS. This should be included as a
-#' dependency for custom tutorial formats that wish to ensure that that
+#' dependency for custom tutorial formats that wish to ensure that
 #' tutorial.js and tutorial.css are loaded prior their own scripts and stylesheets.
 #'
 #' @export
@@ -10,7 +10,7 @@ ISDS_html_dependency <- function() {
   htmltools::htmlDependency(
     name = "tutorial",
     version = utils::packageVersion("ISDStutorials"),
-    src = html_dependency_src("lib", "tutorial"),
+    src = learnr:::html_dependency_src("lib", "tutorial"),
     script = list(src = "print-format.js"),
     stylesheet = list(src = "print-format.css"),
     all_files = TRUE
