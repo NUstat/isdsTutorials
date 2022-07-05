904cb200cc06827f667dfac1a011af9ddf2e3cc9
dsass1 <dsass1@comcast.net>
2022-07-05 15:01:06 -0500

    add grade recorder

---
+++ R/fun_recorder.R
@@ -0,0 +1,107 @@
+#' @title Tutorial grade recorder
+#'
+#' @description
+#' The following function was modified from the learnr package, available at
+#' https://github.com/rstudio/learnr. Many thanks to those who
+#' developed a fantastic tool for courses that teach R and use the learnr
+#' package.
+#'
+#' The following isds recorder uses the server functions to store
+#' historical user's submissions
+#' @export
+isds_recorder <- function(tutorial_id, tutorial_version,
+                         user_id, event, data) {
+  if(event %in% c("question_submission", "exercise_result") &&
+     data$label != "grade_recorder" ){
+
+    tmp <- paste0(tutorial_id,",", tutorial_version, ", ",
+                  user_id, ", ", Sys.time() ,", ",
+                  event, ", ", data$label, ", ",
+                  #data$answer, ", ",
+                  as.numeric(data$correct),
+                  as.numeric(data$feedback$correct), sep = "")
+
+    callModule(
+      recorder_module_server,
+      "grade_recorder",
+      record = tmp
+    )
+  }
+}
+
+#' @export
+recorder_module_server <- function(input, output, session, record) {
+
+  question_state <- reactiveVal()
+
+  observeEvent(
+    req(session$userData$learnr_state() == "restored"),
+    once = TRUE,
+    recorder_module_server_impl(input, output, session, record, question_state)
+  )
+}
+
+#' @export
+recorder_module_server_impl <- function(
+    input, output, session,
+    record,
+    question_state = NULL
+) {
+
+  ns <- getDefaultReactiveDomain()$ns
+  # set a seed for each user session for question methods to use
+  #record$seed <- learnr:::random_seed()
+
+  # only set when a submit button has been pressed
+  # (or reset when try again is hit)
+  # (or set when restoring)
+  update_grade <- reactiveVal(NULL, label = "grade_recorder")
+
+  is_correct_info <- NULL
+  is_done <- NULL
+
+  # restore past submission
+  #  If no prior submission, it returns NULL
+  past_submission_answer <- learnr:::retrieve_question_submission_answer(session, "grade_recorder")
+  # initialize like normal... nothing has been submitted
+  #   or
+  # initialize with the past answer
+  update_grade(past_submission_answer)
+
+  if(!is.null(record)){
+      newval <- paste0(update_grade(), record,
+                       collapse = "\n", sep = "//")
+
+      update_grade(newval)
+  }
+
+  # output$grade_record <- renderText({
+  #   update_grade()
+  # })
+
+  observe( {
+    # submit question to server
+    learnr:::event_trigger(
+      session = session,
+      event   = "question_submission",
+      data    = list(
+        label    = "grade_recorder",
+        question = NULL,
+        answer   = update_grade(),
+        correct  = NULL
+      )
+    )
+
+  })
+
+  data = list(
+        label    = "grade_recorder",
+        question = NULL,
+        answer   = update_grade(),
+        correct  = NULL
+      )
+
+  learnr:::set_tutorial_state("grade_recorder", data)
+
+  return(update_grade() )
+}
---
+++ R/grade_tutorial.R
@@ -0,0 +1,148 @@
+#' Tutorial grade options
+#'
+#' @description
+#' Obtain grade on all question and exercise submissions for a user.
+#'
+#' Shiny ui and server logic for the grade computation.
+#'
+#' Note that when including these functions in a learnr Rmd document it is necessary that
+#' the server function, `grade_server()`, be included in an R chunk where `context="server"` as
+#' they interact with the underlying Shiny functionality. Conversely, the ui function,
+#' `grade_ui()`, must *not* be included in an R chunk with a `context`.
+#'
+#' @param label Label to appear on the submit grade button
+#'
+#' @export
+#print grade
+grade_ui <- function(id, label = "grade tutorial") {
+  ns <- NS(id)
+  tagList(
+
+    actionButton( ns("button"), label = label),
+    br(),
+    htmlOutput(ns("pctout")),
+    br(),
+    gt_output(ns("tableout"))
+
+  )
+}
+
+# Define the server logic for a module to compute grade
+#' @param rubric_list A data frame containing a vector of question names and a vector of points_possible for each question
+#' @param num_try Number of tries allowed before grade deduction on that question. Default is 3.
+#' @param deduction The percent (as a decimal) to be deducted for each additional incorrect attempt. Default is 0.1.
+#' @param display One of c("both", "itemize", "percent")
+#'
+#' @export
+grade_server <- function(id, rubric_list, num_try = 3, deduction = 0.1, display = "both") {
+  moduleServer(
+    id,
+    function(input, output, session) {
+
+    observeEvent(input$button, {
+
+      ns <- getDefaultReactiveDomain()$ns
+
+      update_grade <- reactiveVal(NULL, label = "grade_recorder")
+
+      # restore past submission
+      past_submission_answer <- learnr:::retrieve_question_submission_answer(session, "grade_recorder")
+      update_grade(past_submission_answer)
+
+      get_grades <- grade_tutorial(submissions = update_grade() ,
+                     rubric_list = rubric_list)
+
+      output$tableout <- render_gt({
+        if(display != "percent"){
+          get_grades$grade_table
+        }
+
+      })
+
+      output$pctout <- renderText({
+        if(display != "itemize"){
+          paste0('<span style=\"font-size:30px; font-weight:normal; color:red\">',
+                 get_grades$grade_percent, "%")
+        }
+
+      })
+
+      })
+
+    }
+  )
+}
+
+
+#create a table of grades and calculate overall percent
+#' @export
+grade_tutorial <- function(submissions, rubric_list,
+                           num_try = 3, deduction = 0.1){
+
+  table <- submissions %>%
+    data.table() %>%
+    transpose() %>%
+    separate_rows(V1, sep = "//") %>%
+    transpose() %>%
+    tstrsplit(split = ",", names = TRUE)
+
+  # issue using dplyr with shiny object
+  # so need to manually set as table
+  newdf <- data.frame(id = table$V3, time = table$V4,
+                      type = table$V5,
+                      question = table$V6,
+                      correct = table$V7)
+
+  # create data frame of student submission summary
+  correct_q <- newdf %>%
+    filter(!is.na(question)) %>%
+    # remove punctuation for merge
+    mutate(question = str_replace_all(question,
+                                      "[[:punct:]]", "")) %>%
+    # Remove blank space for merge
+    mutate(question = gsub(" ", '', question)) %>%
+    # get counts of attempts
+    count(question, correct) %>%
+    pivot_wider(names_from = correct, values_from = n)
+
+  # match student progress with all possible questions
+  # rubric_list must be defined by creator
+  grade_summary <- left_join(rubric_list, correct_q) %>%
+    janitor::clean_names()
+
+  # create placeholder column if x1 or x0 has not applied yet
+  if(!"x1" %in% colnames(grade_summary)){
+    grade_summary$x1 <- rep(0, length(grade_summary$question))
+  }
+  if(!"x0" %in% colnames(grade_summary)){
+    grade_summary$x0 <- rep(0, length(grade_summary$question))
+  }
+
+  # replace na values with 0
+  grade_summary[is.na(grade_summary$x1),"x1"] <- 0
+  grade_summary[is.na(grade_summary$x0),"x0"] <- 0
+
+  # create summary columns for num_attempts and score
+  grade_organized <- grade_summary %>%
+    mutate(num_attempts = rowSums(select(.,contains("x")),
+                                  na.rm = TRUE)) %>%
+    mutate(score = ifelse(num_attempts>num_try,
+                          points_possible*x1*(1-deduction*(num_attempts-num_try)),
+                          points_possible*x1) ) %>%
+    select(-x1, -x0)
+
+  grade_table <- grade_organized %>%
+    gt(rowname_col = "question") %>%
+    grand_summary_rows(
+                  columns = c(points_possible, score),
+                  fns = list(
+                    total = ~sum(.)
+                  ),
+                  formatter = fmt_number)
+
+  grade_percent = round(100*sum(grade_organized$score)/sum(grade_organized$points_possible),2)
+
+  return(list(grade_table = grade_table, grade_percent = grade_percent))
+
+}
+
