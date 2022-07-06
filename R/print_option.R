4a98b26c962ff72d82a78b712d1d3582f18be9db
dsass1 <dsass1@comcast.net>
2022-07-06 13:20:46 -0500

    major changes

---
+++ R/print_option.R
@@ -0,0 +1,18 @@
+#' Tutorial print option
+#'
+#' @description
+#' Print the tutorial. Should load print-file.css into tutorial yaml
+#' for nice looking document.
+#' @param label Label to appear on the print button
+#'
+#' @export
+#print grade
+print_ui <- function(label = "print tutorial") {
+  #Print JS uses CSS formatting
+  jscode <- HTML("$(document).on('shiny:inputchanged', function(event){ if (event.name === 'printButton') {window.print();}});")
+
+  shinyjs::useShinyjs()
+  shinyjs::extendShinyjs(text = jscode,functions = c())
+  actionButton("printButton", label = label, style="opacity: .7; color: #000;")
+
+}
