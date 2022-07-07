2ca8557e5fd8cee9a746062c40ab277fa5be7ac2
dsass1 <dsass1@comcast.net>
2022-07-07 17:14:05 -0500

    add js

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(...) {
   shiny::addResourcePath(
-    prefix = "css", # custom prefix that will be used to reference your directory
+    prefix = "assets", # custom prefix that will be used to reference your directory
     directoryPath = system.file("www", package = "ISDStutorials") # path to resource in your package
   )
 }
