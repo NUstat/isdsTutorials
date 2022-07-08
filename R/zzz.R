7a5dfb2a50f86a1db45b7973d869bd116a838184
dsass1 <dsass1@comcast.net>
2022-07-08 14:46:35 -0500

    move dependencies

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,19 @@
 .onLoad <- function(...) {
-  shiny::addResourcePath(
-    prefix = "assets", # custom prefix that will be used to reference your directory
-    directoryPath = system.file("www", package = "ISDStutorials") # path to resource in your package
+  # shiny::addResourcePath(
+  #   prefix = "assets", # custom prefix that will be used to reference your directory
+  #   directoryPath = system.file("www", package = "ISDStutorials") # path to resource in your package
+  # )
+
+  htmltools::attachDependencies(
+    "print",
+    htmltools::htmlDependency(
+      name = "print",
+      version = utils::packageVersion("ISDStutorials"),
+      src = system.file("www", package = "ISDStutorials"),
+      script = "print-format.js",
+      stylesheet = "print-format.css",
+      all_files = TRUE
+    )
   )
 }
 
