7e0dac790eeee10afc7c864614dd726840328f69
dsass1 <dsass1@comcast.net>
2022-07-29 10:04:19 -0500

    add multiple blank question option

---
+++ R/question_blank.R
@@ -0,0 +1,515 @@
+#' Fill in the blank question for learnr tutorials.
+#'
+#' Add interactive fill in the blank tasks to your `learnr` tutorials.
+#' Note: you must include a single blank line ___ in the question text.
+#'
+#' @param placeholder Sample text to appear in blank.
+#' @param rows,cols Defines the size of the text input area in terms of the
+#'   number of rows or character columns visible to the user. If either `rows`
+#'   or `cols` are provided, the quiz input will use [shiny::textAreaInput()]
+#'   for the text input, otherwise the default input element is a single-line
+#'   [shiny::textInput()].
+#' @param trim Logical to determine if whitespace before and after the answer
+#'   should be removed.  Defaults to `TRUE`.
+#' @param ... parameters passed onto learnr answer.
+#' @inheritParams learnr::question
+#'
+#' @return A custom `learnr` question, with `type = blank`.
+#'
+# @return Returns a learnr question of type `"blank"`.
+#'
+#' @export
+question_blank <- function(
+  text,
+  ...,
+  type = "blank",
+  #correct = paste0(icons::ionicons("checkmark-outline") ),
+  #incorrect = paste0(icons::ionicons("close-outline") ),
+  correct = "Correct!",
+  incorrect = "Incorrect",
+  try_again = incorrect,
+  allow_retry = FALSE,
+  random_answer_order = FALSE,
+  placeholder = "Enter answer here...",
+  trim = TRUE,
+  rows = NULL,
+  cols = NULL,
+  options = list()
+) {
+  checkmate::assert_character(placeholder, len = 1, null.ok = TRUE, any.missing = FALSE)
+  checkmate::assert_logical(trim, len = 1, null.ok = FALSE, any.missing = FALSE)
+
+  question <- blank_question(
+    text = text,
+    ...,
+    type = "blank",
+    correct = correct,
+    incorrect = incorrect,
+    allow_retry = allow_retry,
+    random_answer_order = FALSE,
+    options = utils::modifyList(
+      options,
+      list(
+        placeholder = placeholder,
+        trim = trim,
+        rows = rows,
+        cols = cols
+      )
+    )
+  )
+    question
+
+}
+
+
+blank_question <- function(
+    text,
+    ...,
+    type = c("blank"),
+    correct = "Correct!",
+    incorrect = "Incorrect",
+    try_again = incorrect,
+    message = NULL,
+    post_message = NULL,
+    loading = c("**Loading:** ", format(text), "<br/><br/><br/>"),
+    submit_button = rlang::missing_arg(),
+    try_again_button = rlang::missing_arg(),
+    allow_retry = FALSE,
+    random_answer_order = FALSE,
+    options = list()
+) {
+
+  # one time tutor initialization
+  #initialize_tutorial()
+
+  # capture/validate answers
+  ellipsis::check_dots_unnamed() # validate all answers are not named and not a misspelling
+  answers <- list(...)
+  
+  # ensure num blanks in 1:5
+  split <- unlist(stringr::str_split(text, pattern = "___") )
+  num_blank = length(split) - 1
+  if( !num_blank %in% c(1:5) ){
+    stop("Number of blanks must be equal to 1, 2, 3, 4, or 5.")
+  }
+  # ensure answer choices = num blank
+  if (num_blank != length(answers)) {
+    stop("Number of blanks must equal number of answer() inputs.")
+  }
+  
+  # can not guarantee that `label` exists
+  label <- knitr::opts_current$get('label')
+  q_id <- label %||% learnr:::random_question_id()
+
+  # i18nize button labels if default values are used
+  submit_button <-
+    if (rlang::is_missing(submit_button)) {
+      learnr:::i18n_span("button.questionsubmit", "Submit Answer")
+    } else {
+      learnr:::quiz_text(submit_button)
+    }
+
+  try_again_button <-
+    if (rlang::is_missing(try_again_button)) {
+      learnr:::i18n_span("button.questiontryagain", "Try Again")
+    } else {
+      learnr:::quiz_text(try_again_button)
+    }
+
+  ret <- list(
+    type = type,
+    label = label,
+    question = learnr:::quiz_text(text),
+    answers = answers,
+    button_labels = list(
+      submit = submit_button,
+      try_again = try_again_button
+    ),
+    messages = list(
+      correct = learnr:::quiz_text(correct),
+      try_again = learnr:::quiz_text(try_again),
+      incorrect = learnr:::quiz_text(incorrect),
+      message = learnr:::quiz_text(message),
+      post_message = learnr:::quiz_text(post_message)
+    ),
+    ids = list(
+      answer = NS(q_id)("answer"),
+      question = q_id
+    ),
+    loading = learnr:::quiz_text(loading),
+    random_answer_order = random_answer_order,
+    allow_retry = allow_retry,
+    # Set a seed for local testing, even though it is overwritten for each shiny session
+    seed = learnr:::random_seed(),
+    options = options
+  )
+  #class(ret) <- c(type, "notes_question")
+  class(ret) <- c(type, "tutorial_question")
+  ret
+}
+
+
+#' @export
+#' @seealso question_blank
+question_ui_initialize.blank <- function(question, value, ...) {
+  
+  #split question by blanks
+  split <- unlist(stringr::str_split(question$question, pattern = "___") )
+  
+  pos <- NULL
+  for(i in 1:length(split)){
+   if(i < length(split)){
+     pos <- c(pos, split[i], "___")
+   }
+   else{
+     pos <- c(pos, split[i])
+   }
+  }
+  num_blank = length(split)-1
+  num_pos = length(pos)
+  
+  # set output to previous answers
+  if (!is.null(value)) {
+    ans <- as.character( unlist(value) )
+  } else {
+    ans <- rep("", num_blank)
+  }
+  
+  # set unique ids
+  # pretty close to unique id
+  rand = paste0(sample.int(100,1), sample.int(100,1) )
+  # set input and bucket ids
+  input_ids = lapply(seq(1,num_blank), function(x) paste0("blank", rand, x) )
+  css_ids = lapply(seq(1,num_blank), function(x) paste0("text", rand, x) )
+  other_ids = lapply(seq(1,num_pos), function(x) paste0("other", rand, x) )
+  group_ids = paste0("group", rand)
+  
+
+  bootstrapPage(
+    div(id = question$ids$answer,
+        class = "bucketList",
+        
+      lapply(seq(1,num_pos), function(x)
+        # cannot figure out how to make Shiny.setInputValue a function of numBlanks
+        # so setting it as a condition with max blanks of 5
+        if(num_blank == 1 && pos[x] == "___"){
+          tags$div(
+            class = "panel-body",
+            style="display:inline-block",
+            id = css_ids[x/2],
+            tags$input(type = "text", 
+                       id = paste0(input_ids[x/2]), 
+                       class = "textClass",
+                       value = ans[x/2],
+                       onkeyup = "Shiny.setInputValue(document.getElementsByClassName('bucketList')[0].id, 
+                       [document.getElementsByClassName('textClass')[0].value] )" )
+          )
+        }else if(num_blank == 2 && pos[x] == "___"){
+          tags$div(
+            class = "panel-body",
+            style="display:inline-block",
+            id = css_ids[x/2],
+            tags$input(type = "text", 
+                       id = paste0(input_ids[x/2]), 
+                       class = "textClass",
+                       value = ans[x/2],
+                       onkeyup = "Shiny.setInputValue(document.getElementsByClassName('bucketList')[0].id, 
+                       [document.getElementsByClassName('textClass')[0].value,
+                   document.getElementsByClassName('textClass')[1].value] )" )
+          )
+        }else if(num_blank == 3 && pos[x] == "___"){
+          tags$div(
+            class = "panel-body",
+            style="display:inline-block",
+            id = css_ids[x/2],
+            tags$input(type = "text", 
+                       id = paste0(input_ids[x/2]), 
+                       class = "textClass",
+                       value = ans[x/2],
+                       onkeyup = "Shiny.setInputValue(document.getElementsByClassName('bucketList')[0].id, 
+                       [document.getElementsByClassName('textClass')[0].value,
+                   document.getElementsByClassName('textClass')[1].value,
+                   document.getElementsByClassName('textClass')[2].value] )" )
+          )
+        }else if(num_blank == 4 && pos[x] == "___"){
+          tags$div(
+            class = "panel-body",
+            style="display:inline-block",
+            id = css_ids[x/2],
+            tags$input(type = "text", 
+                       id = paste0(input_ids[x/2]), 
+                       class = "textClass",
+                       value = ans[x/2],
+                       onkeyup = "Shiny.setInputValue(document.getElementsByClassName('bucketList')[0].id, 
+                       [document.getElementsByClassName('textClass')[0].value,
+                   document.getElementsByClassName('textClass')[1].value,
+                   document.getElementsByClassName('textClass')[2].value,
+                   document.getElementsByClassName('textClass')[3].value] )" )
+          )
+        }else if(num_blank == 5 && pos[x] == "___"){
+          tags$div(
+            class = "panel-body",
+            style="display:inline-block",
+            id = css_ids[x/2],
+            tags$input(type = "text", 
+                       id = paste0(input_ids[x/2]), 
+                       class = "textClass",
+                       value = ans[x/2],
+                       onkeyup = "Shiny.setInputValue(document.getElementsByClassName('bucketList')[0].id, 
+                       [document.getElementsByClassName('textClass')[0].value,
+                   document.getElementsByClassName('textClass')[1].value,
+                   document.getElementsByClassName('textClass')[2].value,
+                   document.getElementsByClassName('textClass')[3].value,
+                   document.getElementsByClassName('textClass')[4].value] )" )
+          )
+        }else{
+          div(
+            class = "panel-body",
+            style="display:inline-block",
+            id = other_ids[x],
+            p(pos[x])
+          )
+        }
+        ) #end lapply
+     ) #ends bucket div group
+    
+  ) #end bootstrap page
+  
+  
+  
+}
+
+
+#' @export
+#' @seealso question_blank
+question_is_valid.blank <- function(question, value, ...) {
+   #value <- as.character( unlist(value) )
+   split <- unlist(stringr::str_split(question$question, pattern = "___") )
+   num_blank = length(split) - 1
+   
+   if (is.null(value)) {
+     return(FALSE)
+   }else if(length(value) < num_blank ){
+     return(FALSE)
+   }
+   
+   if (isTRUE(question$options$trim)) {
+     return(min( nchar(stringr::str_trim(value)) ) > 0)
+   } else{
+     return(min( nchar(value) ) > 0)
+   }
+   # 
+   # else if(min(nchar(stringr::str_trim(value))) > 0){
+   #   return(TRUE)
+   # }
+   
+  # if(length(value) < num_blank){
+  #   return(FALSE)
+  # }else if(isTRUE(question$options$trim)) {
+  #   return(min(nchar(stringr::str_trim(value))) > 0)
+  # } else{
+  #   #return(nchar(value) > 0)
+  #   return(min(nchar(value) ) > 0)
+  # }
+   
+}
+
+#' @export
+#' @seealso question_blank
+question_is_correct.blank <- function(question, value, ...) {
+  
+  append_message <- function(x, ans) {
+    message <- ans$message
+    if (is.null(message)) {
+      return(x)
+    }
+    if (length(x) == 0) {
+      message
+    } else {
+      tagList(x, message)
+    }
+  }
+  
+  #how is value being taken in
+  if (min(nchar(value)) == 0) {
+    if (!is.null(shiny::getDefaultReactiveDomain())) {
+      showNotification("Please enter some text before submitting", type = "error")
+    }
+    #shiny::validate("Please enter some text")
+    value
+  }
+
+  if (isTRUE(question$options$trim)) {
+    value <- stringr::str_trim(value)
+  }
+
+  
+  for(i in 1:length(value)){
+    q_ans <- question$answers[[i]]
+    v_ans <- value[i]
+    #check if answer is a value or function
+    if(q_ans$type == "function"){
+      answer_checker <- eval(parse(text = q_ans$value), envir = rlang::caller_env())
+      ret <- answer_checker(v_ans)
+      if (inherits(ret, "learnr_mark_as")) {
+        return(ret)
+      }
+    } else{ # literal check
+      if (v_ans == q_ans$value){ # correct
+        value_is_correct <- TRUE
+      } else{ # wrong
+        value_is_correct <- FALSE
+        # do not check remaining answers
+        break
+      }
+    }
+  }
+    
+  ret_messages <- c()
+  q_answers <- learnr:::answers_split_type(question$answers)
+  if (value_is_correct) {
+    # selected all correct answers. get all good messages as all correct answers were selected
+    for (q_answer in q_answers[["literal"]]) {
+      if (q_answer$correct) {
+        ret_messages <- append_message(ret_messages, q_answer)
+      }
+    }
+  }
+  
+  learnr::mark_as(value_is_correct, ret_messages)
+  
+}
+
+#' @export
+#' @seealso question_blank
+question_ui_completed.blank <- function(question, value, ...) {
+  # TODO display correct values with X or √ compared to best match
+  # TODO DON'T display correct values (listen to an option?)
+  
+  ns <- NS(question$ids$question)
+  
+  #split question by blanks
+  split <- unlist(stringr::str_split(question$question, pattern = "___") )
+  
+  pos <- NULL
+  for(i in 1:length(split)){
+    if(i < length(split)){
+      pos <- c(pos, split[i], "___")
+    }
+    else{
+      pos <- c(pos, split[i])
+    }
+  }
+  num_blank = length(split)-1
+  num_pos = length(pos)
+  
+  # set output to previous answers
+  if (!is.null(value)) {
+    ans <- as.character( unlist(value) )
+  } else {
+    ans <- rep("", num_blank)
+  }
+  
+  # set unique ids
+  # pretty close to unique id
+  rand = paste0(sample.int(100,1), sample.int(100,1) )
+  # set input and bucket ids
+  input_ids = lapply(seq(1,num_blank), function(x) paste0("blank", rand, x) )
+  css_ids = lapply(seq(1,num_blank), function(x) paste0("text", rand, x) )
+  other_ids = lapply(seq(1,num_pos), function(x) paste0("other", rand, x) )
+  group_ids = paste0("group", rand)
+  
+  learnr::disable_all_tags(
+  bootstrapPage(
+    div(id = question$ids$answer,
+        class = "bucketList",
+        
+        lapply(seq(1,num_pos), function(x)
+          # cannot figure out how to make Shiny.setInputValue a function of numBlanks
+          # so setting it as a condition with max blanks of 5
+          if(num_blank == 1 && pos[x] == "___"){
+            tags$div(
+              class = "panel-body",
+              style="display:inline-block",
+              id = css_ids[x/2],
+              tags$input(type = "text", 
+                         id = paste0(input_ids[x/2]), 
+                         class = "textClass",
+                         value = ans[x/2],
+                         onkeyup = "Shiny.setInputValue(document.getElementsByClassName('bucketList')[0].id, 
+                       [document.getElementsByClassName('textClass')[0].value] )" )
+            )
+          }else if(num_blank == 2 && pos[x] == "___"){
+            tags$div(
+              class = "panel-body",
+              style="display:inline-block",
+              id = css_ids[x/2],
+              tags$input(type = "text", 
+                         id = paste0(input_ids[x/2]), 
+                         class = "textClass",
+                         value = ans[x/2],
+                         onkeyup = "Shiny.setInputValue(document.getElementsByClassName('bucketList')[0].id, 
+                       [document.getElementsByClassName('textClass')[0].value,
+                   document.getElementsByClassName('textClass')[1].value] )" )
+            )
+          }else if(num_blank == 3 && pos[x] == "___"){
+            tags$div(
+              class = "panel-body",
+              style="display:inline-block",
+              id = css_ids[x/2],
+              tags$input(type = "text", 
+                         id = paste0(input_ids[x/2]), 
+                         class = "textClass",
+                         value = ans[x/2],
+                         onkeyup = "Shiny.setInputValue(document.getElementsByClassName('bucketList')[0].id, 
+                       [document.getElementsByClassName('textClass')[0].value,
+                   document.getElementsByClassName('textClass')[1].value,
+                   document.getElementsByClassName('textClass')[2].value] )" )
+            )
+          }else if(num_blank == 4 && pos[x] == "___"){
+            tags$div(
+              class = "panel-body",
+              style="display:inline-block",
+              id = css_ids[x/2],
+              tags$input(type = "text", 
+                         id = paste0(input_ids[x/2]), 
+                         class = "textClass",
+                         value = ans[x/2],
+                         onkeyup = "Shiny.setInputValue(document.getElementsByClassName('bucketList')[0].id, 
+                       [document.getElementsByClassName('textClass')[0].value,
+                   document.getElementsByClassName('textClass')[1].value,
+                   document.getElementsByClassName('textClass')[2].value,
+                   document.getElementsByClassName('textClass')[3].value] )" )
+            )
+          }else if(num_blank == 5 && pos[x] == "___"){
+            tags$div(
+              class = "panel-body",
+              style="display:inline-block",
+              id = css_ids[x/2],
+              tags$input(type = "text", 
+                         id = paste0(input_ids[x/2]), 
+                         class = "textClass",
+                         value = ans[x/2],
+                         onkeyup = "Shiny.setInputValue(document.getElementsByClassName('bucketList')[0].id, 
+                       [document.getElementsByClassName('textClass')[0].value,
+                   document.getElementsByClassName('textClass')[1].value,
+                   document.getElementsByClassName('textClass')[2].value,
+                   document.getElementsByClassName('textClass')[3].value,
+                   document.getElementsByClassName('textClass')[4].value] )" )
+            )
+          }else{
+            div(
+              class = "panel-body",
+              style="display:inline-block",
+              id = other_ids[x],
+              p(pos[x])
+            )
+          }
+        ) #end lapply
+    ) #ends bucket div group
+    
+  ) #end bootstrap page
+  
+  ) #end disable all tags
+  
+}
